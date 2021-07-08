d001be04897e288fbac0592a93e4995d08fa87ff
tolehoai <tolehoai@gmail.com>
2021-07-08 17:06:28 +0700

    macOS

---
+++ react-form/src/components/input-field/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+InputField.propTypes = {
+    
+};
+
+function InputField(props) {
+    return (
+        <div>
+            input
+        </div>
+    );
+}
+
+export default InputField;
